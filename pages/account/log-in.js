8ba9bdad91d6541785910981acf1f2355a1ef1fb
cornerbodega <merhone@gmail.com>
2023-08-11 04:24:29 -0700

    Update Auth0 env

--- pages/account/log-in.js
+++ pages/account/log-in.js
@@ -1,46 +1,7 @@
 import Auth0LoginButtons from "../../components/Auth0LoginButtons";
 
 function LogIn() {
-  // return <></>;
-  // return <a href="/api/auth/login">Login</a>;
-  //
   return <Auth0LoginButtons></Auth0LoginButtons>;
-  // if (!session) {
-  // } else {
-  //   return <div>Logged in! {JSON.stringify(session)}</div>;
-  // }
-  // return <></>;
-  // const [session, setSession] = useState(null);
-
-  // useEffect(() => {
-  //   supabase.auth.getSession().then(({ data: { session } }) => {
-  //     setSession(session);
-  //   });
-
-  //   const {
-  //     data: { subscription },
-  //   } = supabase.auth.onAuthStateChange((_event, session) => {
-  //     setSession(session);
-  //   });
-
-  //   return () => subscription.unsubscribe();
-  // }, []);
-
-  // if (!session) {
-  //   return <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />;
-  // } else {
-  //   return <div>Logged in!</div>;
-  // }
-  // <div>
-
-  //     <Row className="text-primary">
-  //       <h5 className="mb-3 mt-3">Log In</h5>
-  //       <Col>
-
-  //       </Col>
-  //     </Row>
-
-  //   </div>
 }
 
 export default LogIn;
