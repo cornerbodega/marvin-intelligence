2c681e366c42ec5b9aec5bc39203c924ac5f8441
cornerbodega <merhone@gmail.com>
2023-09-11 14:50:05 -0700

    Update create-agent-endpoint.js

--- pages/api/agents/create-agent-endpoint.js
+++ pages/api/agents/create-agent-endpoint.js
@@ -29,34 +29,36 @@ export async function createAgentFunction({
   };
   const existingAgentsArray = req.body.existingAgentNames;
   // console.log("existingAgentsArray");
-  const expertiseCompletion = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [
-      // {
-      //   role: "system",
-      //   content:
-      //     "You are an expert at identifying areas of expertise that will be beneficial for further research on a given topic. You always return results as a JSON array of strings. Arrays have at least one element and can have up to three. Provide the JSON objects directly without additional explanations.",
-      // },
-      {
-        role: "system",
-        content:
-          "Your task is to identify up to three areas of expertise beneficial for further research on a given topic. Always return the results directly as a raw JSON array of strings, containing between one and three elements. Do not wrap the array in an object or provide any additional explanations or keys. Respond directly with the array.",
-      },
-      {
-        role: "user",
-        content: `making software that makes millions of dollars`,
-      },
+  const expertiseCompletion = await openai
+    .createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        // {
+        //   role: "system",
+        //   content:
+        //     "You are an expert at identifying areas of expertise that will be beneficial for further research on a given topic. You always return results as a JSON array of strings. Arrays have at least one element and can have up to three. Provide the JSON objects directly without additional explanations.",
+        // },
+        {
+          role: "system",
+          content:
+            "Your task is to identify up to three areas of expertise beneficial for further research on a given topic. Always return the results directly as a raw JSON array of strings, containing between one and three elements. Do not wrap the array in an object or provide any additional explanations or keys. Respond directly with the array.",
+        },
+        {
+          role: "user",
+          content: `making software that makes millions of dollars`,
+        },
 
-      {
-        role: "assistant",
-        content: `["Software development", "Entrepreneurship", "Product management"]`,
-      },
-      {
-        role: "user",
-        content: `${req.body.expertiseInput} `,
-      },
-    ],
-  });
+        {
+          role: "assistant",
+          content: `["Software development", "Entrepreneurship", "Product management"]`,
+        },
+        {
+          role: "user",
+          content: `${req.body.expertiseInput} `,
+        },
+      ],
+    })
+    .catch((error) => console.error(error));
 
   const expertiseResponse = JSON.parse(
     expertiseCompletion.data.choices[0].message.content
@@ -100,35 +102,37 @@ export async function createAgentFunction({
     if (req.body.specializedTraining) {
       specializedTrainingString = `Agent is trained to know ${req.body.specializedTraining}.`;
     }
-    const chat_completion = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: [
-        {
-          role: "system",
-          content:
-            "You are an expert at knowing lots of animal names and their characteristics. You are always as specific as possible with the sub-species. Return Emperor Penuin, instead of Penguin, Golden Retriever, instead of Dog.",
-        },
-        {
-          role: "user",
-          content: `Which animal embodies the characteristics of Writing Apps, and System Design, and React? Do not explain your answer. Return only the results in the following JSON format.`,
-        },
-        {
-          role: "assistant", //try 'assistant' as well, but this works so far
-          // content: `{
-          //     "animal": "Fennec Fox",
-          //     "bio": "The Fennec fox is often seen as a symbol of cunning and adaptability. In the realm of business strategy and market analysis, these are crucial traits. Foxes are known to observe and analyze their environment keenly, strategize their hunt, and change tactics as necessary, demonstrating a mix of intelligence, foresight, and flexibility. In business, the ability to assess the market, anticipate changes, and adjust strategies accordingly is vital for success, much like a fox navigating its terrain and seeking its prey."
-          //   }`,
-          content: `{
+    const chat_completion = await openai
+      .createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "system",
+            content:
+              "You are an expert at knowing lots of animal names and their characteristics. You are always as specific as possible with the sub-species. Return Emperor Penuin, instead of Penguin, Golden Retriever, instead of Dog.",
+          },
+          {
+            role: "user",
+            content: `Which animal embodies the characteristics of Writing Apps, and System Design, and React? Do not explain your answer. Return only the results in the following JSON format.`,
+          },
+          {
+            role: "assistant", //try 'assistant' as well, but this works so far
+            // content: `{
+            //     "animal": "Fennec Fox",
+            //     "bio": "The Fennec fox is often seen as a symbol of cunning and adaptability. In the realm of business strategy and market analysis, these are crucial traits. Foxes are known to observe and analyze their environment keenly, strategize their hunt, and change tactics as necessary, demonstrating a mix of intelligence, foresight, and flexibility. In business, the ability to assess the market, anticipate changes, and adjust strategies accordingly is vital for success, much like a fox navigating its terrain and seeking its prey."
+            //   }`,
+            content: `{
                 "animal": "Honeybee",
                 "bio": "With a codebase that draws inspiration from one of nature's most impressive architects, this AI agent is here to ensure your tech solutions are nothing short of hive-quality excellence. Approach with curiosity, leave with clarity."
             }`,
-        },
-        {
-          role: "user",
-          content: `Which animal embodies the characteristics of ${expertiseString}? ${specializedTrainingString} ${cannotBeAnimalsString}. Return your answer in the following JSON format: {animal: "Animal Name", bio: "Animal Bio"}`,
-        },
-      ],
-    });
+          },
+          {
+            role: "user",
+            content: `Which animal embodies the characteristics of ${expertiseString}? ${specializedTrainingString} ${cannotBeAnimalsString}. Return your answer in the following JSON format: {animal: "Animal Name", bio: "Animal Bio"}`,
+          },
+        ],
+      })
+      .catch((error) => console.error(error));
     const animalNameResponseContent =
       chat_completion.data.choices[0].message.content;
     if (animalNameResponseContent) {
@@ -155,7 +159,9 @@ export async function createAgentFunction({
   // }
   // let existingAgentsArray = req.body.existingAgentNames;
   async function getAgentName() {
-    let agentNameResponse = await generateAnimalName();
+    let agentNameResponse = await generateAnimalName().catch((error) =>
+      console.error(error)
+    );
     return agentNameResponse;
     // if (existingAgentsArray) {
     //   if (existingAgentsArray.length > 0) {
@@ -174,7 +180,9 @@ export async function createAgentFunction({
     //   return agentNameResponse;
     // }
   }
-  const agentNameResponse = await getAgentName();
+  const agentNameResponse = await getAgentName().catch((error) =>
+    console.error(error)
+  );
   // console.log(`existingAgentsArray.includes(agentNameResponse.animalName)`);
   // console.log(
   // `${existingAgentsArray}.includes(${agentNameResponse.animalName})`
